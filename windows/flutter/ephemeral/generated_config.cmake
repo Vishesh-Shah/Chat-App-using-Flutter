ee589b4dfbb3fdd26b304e93eccf01ce107dd01b
Vishesh-Shah <137981598+Vishesh-Shah@users.noreply.github.com>
2023-07-26 14:59:43 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\FLUTTER\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\vissh\\Documents\\Chat_app\\app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\FLUTTER\\flutter"
+  "PROJECT_DIR=C:\\Users\\vissh\\Documents\\Chat_app\\app"
+  "FLUTTER_ROOT=D:\\FLUTTER\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\vissh\\Documents\\Chat_app\\app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\vissh\\Documents\\Chat_app\\app"
+  "FLUTTER_TARGET=C:\\Users\\vissh\\Documents\\Chat_app\\app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\vissh\\Documents\\Chat_app\\app\\.dart_tool\\package_config.json"
+)
